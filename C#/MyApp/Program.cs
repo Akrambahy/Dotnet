5740d5253686cd94832af6f6832ff828545ffb6a
AkramBahy <akrambahy0@gmail.com>
2026-01-07 01:00:43 +0200

    add basec folders And Explean Data Type in cs

---
+++ C#/MyApp/Program.cs
@@ -0,0 +1,43 @@
+﻿//Data Types In C#
+ //Integers
+   int Number=5;
+    Console.WriteLine("Number : "+ Number);
+
+ //Strings 
+  string firstName="Akram";
+  Console.WriteLine("Hello, "+ firstName);
+ //Numeric 
+   float fNumber=2.2F; // We must put type after Number
+   decimal deNumber=86.2M;
+   double duNumber=2.2D; // default is D as a Double
+  
+  Console.WriteLine("Float Number : "+fNumber);
+  Console.WriteLine("Decimal Number : "+deNumber);
+  Console.WriteLine("Double Number : "+duNumber);
+ //boolean  
+  bool IsWork=false;
+  Console.WriteLine(IsWork);
+ // Type Conversion
+    int intVar=5;
+    double doubleVar=13.6;
+     Console.WriteLine("Integer Number : "+intVar);
+     Console.WriteLine("Double Number : "+doubleVar);
+   //Implicit Conversion
+     Console.WriteLine("After Conversion");
+  //   doubleVar=intVar;
+     Console.WriteLine("Integer Number : "+intVar);
+     Console.WriteLine("Double Number : "+doubleVar);
+
+   //Explicit Conversion  
+
+  Console.WriteLine("After Conversion");
+     intVar=(int)doubleVar;
+     Console.WriteLine("Integer Number : "+intVar);
+     Console.WriteLine("Double Number : "+doubleVar);
+
+
+
+
+
+
+ 
