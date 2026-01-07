e970a413edc3a45a2ccafcf6f8dd320d5d4122b6
AkramBahy <akrambahy0@gmail.com>
2026-01-07 02:48:11 +0200

    Add New Topics

---
+++ C#/DataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿//Data Types In C#
+ //Integers
+   int Number=5;
+    Console.WriteLine("Number : "+ Number);
+
+ //Strings 
+  string firstName="Akram";
+  Console.WriteLine("Hello, "+ firstName);
+ //Numeric 
+   float fNumber=2.2F; // We must put type after Number
+   decimal deNumber=86.2M;
+   double duNumber=2.2D; // default is D as a Double
+  
+  Console.WriteLine("Float Number : "+fNumber);
+  Console.WriteLine("Decimal Number : "+deNumber);
+  Console.WriteLine("Double Number : "+duNumber);
+ //boolean  
+  bool IsWork=false;
+  Console.WriteLine(IsWork);
+
+ 
+
+
+
+
+ 
---
+++ C#/TypeConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿ // Type Conversion
+    int intVar=5;
+    double doubleVar=13.6;
+     Console.WriteLine("Integer Number : "+intVar);
+     Console.WriteLine("Double Number : "+doubleVar);
+   //Implicit Conversion
+     Console.WriteLine("After Conversion");
+  //   doubleVar=intVar;
+     Console.WriteLine("Integer Number : "+intVar);
+     Console.WriteLine("Double Number : "+doubleVar);
+
+   //Explicit Conversion  
+
+  Console.WriteLine("After Conversion");
+     intVar=(int)doubleVar; //without numericPart
+     Console.WriteLine("Integer Number : "+intVar);
+     Console.WriteLine("Double Number : "+doubleVar);
+    // Conversion String
+     string age="19";
+     int intAge=int.Parse(age);
