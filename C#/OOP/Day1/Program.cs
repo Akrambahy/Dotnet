c388e784cf3d36c8e1239df73ecac1c7524abb31
AkramBahy <akrambahy0@gmail.com>
2026-01-27 06:31:19 +0200

    add : Day 2

---
+++ C#/OOP/Day1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        class Car
+        {
+            public string brand = "BMW";
+            private string _model = "";
+            private int _speed = 52;
+
+            public void SetModel(string model)
+            {
+                _model = model;
+            }
+
+            public string GetModel()
+            {
+                return _model;
+            }
+
+            public void PrintCarInfo()
+            {
+                Console.WriteLine($"Car Is {brand}:{_model}");
+                Console.WriteLine($"Speed Is {_speed}M/H");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            Car1.SetModel("X6");
+            Car1.PrintCarInfo();
+        }
+    }
+}
