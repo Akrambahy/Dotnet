6709e89872237c830dac8ba644fd280867688ab8
AkramBahy <akrambahy0@gmail.com>
2026-01-28 06:41:57 +0200

    add : Day 3

--- C#/OOP/Day2/Program.cs
+++ C#/OOP/Day2/Program.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+/*
+Program divided To'
+Code Data
+App in Memory Divide To Section For Code And Section For Data
+
+App Loading Process Ram
+
+Data Divided To
+Stack Heap
+Any Local Variable(Variable On Function) Stored In Stack
+
+Sack Stored Block Of Function Mainly Main Fun And Into Add Fun
+Heap Dynamic Allocation Data Stored In RunTime And Removed
+
+Data Types Divide To
+Built In Defined By PL Like int,float,boll
+User Defined DT Defined By User "Like Any Thing Fro Class
+
+Data Type Root
+Value Type Like Struct And Enum Always Have Value
+Reference Type Like Class Store Address like Class RefName=new ClassName(); RefName Is Reference Type of Class  
+ex 
+Car Car1       =     new Car();
+Create object type Car depend on constructor Car
+"Car Car1 " Create Reference Type Of Class Car Store Address Of Class Car Have Name Car1
+"new Car()" Create Object In Memory Of Class Car
+"Car()" Constructor Det how object well build 
+"=" Store Address Of Object Created In Reference Created
+Order Of Execution
+1 Create Object
+2 Call constructor
+
+nots : 
+Ref->Stack
+Object->Heap
+No Ref point To Object Obj Are Dead
+new-> object;
+not operations no initialization
+we must init any local var before any operation 
+build in take value
+user define maybe null
+var members initiated already  (int->0,bool->false)
+
+we call in fun with object with hidden parameter this is reference of object\\
+*/
+namespace Day2
+{
+    class Program
+    {
+        class Car
+        {
+            public string brand = "BMW";
+            private string _model = "";
+            private int _speed = 52;
+
+            public void SetModel(string model)
+            {
+                _model = model;
+            }
+
+            public string GetModel()
+            {
+                return _model;
+            }
+
+            public void PrintCarInfo()
+            {
+                Console.WriteLine($"Car Is {brand}:{_model}");
+                Console.WriteLine($"Speed Is {_speed}M/H");
+            }
+        }
+
+        class Employee
+        {
+            private string _name;
+            public string title;
+            public int salary;
+
+            Employee() // constructor
+            {
+                salary = 3000;
+            }
+
+            public void display(/*hidden parameter this = ref of object*/)
+            {
+                Console.WriteLine($"{_name}:{title}:{_salary}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Car Car1; // Create Ref Of Class Car;
+            new Car(); //  Create Object Car In Memory
+            Car Car2 = new Car(); // store address of obj car created in Car2
+            Employee Employee1 = new Employee(); //Create refrence Employee and store  Create object Type  Employee  depend on constructor Employee
+
+
+        }
+    }
+}
---
+++ C#/OOP/Day3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+/*
+Classes Relations
+1-Association 
+Association Short Relation Are without any Creation For Other Ex Fun Class Call Ref From anther class and Use it
+Anther class are temp for specific Time
+2-Aggregation
+Aggregation Long Relation Are without any Creation For Other Ex Crate Ref on Class And make it point to object From Anther Class And Use it on all Class tile it dead 
+3- Composition
+Composition Long Relation Are with any Creation For Other like Class A Consist Of Class B
+4-Inheritance
+Inheritance Is Relation Between Classes to simulate Is a in life like class1 Is a class2 ex human is a creature
+Logic How Simulate Is a
+syntax How reduce code 
+*/
+namespace Day3
+{
+    class Program
+    {
+        //Association
+        class Pain { }
+        class Board
+        {
+            public void WriteOnBoard(Pain RedPain)
+            {
+
+            }
+        }
+
+        //Aggregation
+
+        class Instructor { }
+
+        class Session
+        {
+            public Instructor? inst; //create Ref Type Of Instructor
+
+            public Session()
+            {
+                inst = null;
+            }
+
+            public void InstructorAttend(Instructor i)
+            {
+                inst = i;
+            }
+            public void InstructorExplain()
+            {}
+                
+
+        }
+
+        //Composition
+        class Head { }
+
+        class Body
+        {
+            public Head head;
+            public Body()
+            {
+                head = new Head();
+            }
+
+            public void Fun1() { }//Use Head
+            public void Fun2() { }//Use Head
+            public void Fun3() { }//Use Head
+        }
+     
+     // Inheritance
+        
+        class Creature
+        {
+           public string name="";
+            public int age=0;
+
+           public void move(){}
+         public  Creature(string Name,int Age){}
+        }
+       class Human:Creature
+        {
+            public string job="";
+            public void learn(){}
+                   public  Human (string Name,string Job,int Age) : base(Name, Age)
+            {
+                
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
