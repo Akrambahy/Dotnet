5ecc7427beebc9644c90ee3cedef713337a06c9f
AkramBahy <akrambahy0@gmail.com>
2026-01-29 04:09:40 +0200

    add : Day4

--- C#/OOP/Day1/obj/Debug/net9.0/Day1.AssemblyInfo.cs
+++ C#/OOP/Day1/obj/Debug/net9.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+950de4cd4470dac880c57d435083183337c7e49e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6709e89872237c830dac8ba644fd280867688ab8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
